6bd04cf9a3a6c8f1b37382d50595060898edbde3
chicoa <lsz18877031499@163.com>
2025-04-23 02:18:48 +0800

    spring mvc return ModelAndView
    spring mvc数据响应-返回ModelAndView对象。

--- lishengzhi_mvc/src/main/java/com/lishengzhi_mvc/controller/UserController.java
+++ lishengzhi_mvc/src/main/java/com/lishengzhi_mvc/controller/UserController.java
@@ -2,6 +2,7 @@ package com.lishengzhi_mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -9,4 +10,19 @@ public class UserController {
     public String save1(){
         return "success.jsp";
     }
+
+    @RequestMapping("save2")
+    public String save2() {
+        return "save2";
+    }
+    @RequestMapping("/save3")
+    public ModelAndView save3(ModelAndView modelAndView){
+        modelAndView.addObject("username","李胜志");
+        modelAndView.addObject("age","21");
+        modelAndView.setViewName("save3.jsp");
+        return modelAndView;
+    }
+
+
+
 }
---
+++ lishengzhi_mvc/target/lishengzhi_mvc/save3.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Kiahim
+  Date: 2025/4/23
+  Time: 1:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@page isELIgnored="false"%>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>我是${username},今年${age}岁。</h1>
+</body>
+</html>
